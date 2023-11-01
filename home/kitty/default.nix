3c535055106b29001b5758089f1c2cbbcb5fc08b
tororutsu <rjahrj@gmail.com>
2023-11-01 08:32:56 -0400

    Working on kitty conf

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -8,8 +8,9 @@
       size = 10;
     };
     settings = {
-     #Windows
-     draw_minimal_borders = true;
+      #Windows
+      draw_minimal_borders = true;
+      tab_title_template =  "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{title}{index}";
 
     };
   };
