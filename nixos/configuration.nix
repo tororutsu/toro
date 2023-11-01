fa6ce11fe5b4dabc510cef551dfdf1c862cbd393
tororutsu <rjahrj@gmail.com>
2023-10-31 20:37:40 -0400

    trying mouse support

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -106,7 +106,7 @@
   hardware.pulseaudio.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.rj = {
