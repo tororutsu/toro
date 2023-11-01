9eee6e54446ded035af00cdbce92ce35c9b7a1c0
tororutsu <rjahrj@gmail.com>
2023-11-01 08:36:00 -0400

    Working on kitty conf

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -10,8 +10,10 @@
     settings = {
       #Windows
       draw_minimal_borders = true;
+      #Tabs
       tab_title_template =  "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{title}{index}";
-
+      #Exit Strat
+      close_on_child_death = true;
     };
   };
 }
