a8eb132f136d7242c8ed87831699eb90c92a6eeb
tororutsu <rjahrj@gmail.com>
2023-11-01 06:27:00 -0400

    trying out keyd config

--- nixos/keyd.nix
+++ nixos/keyd.nix
@@ -1,17 +1,10 @@
 {
   services.keyd = {enable = true;
-    config = ''
-      [ids]
-
-      *
-      
-      [main]
-      
-      # Maps capslock to escape when pressed and control when held.
-      capslock = overload(control, esc)
-      
-      # Remaps the escape key to capslock
-      esc = capslock
-    '';
+    ids = ["*"];
+    settings = {
+      main = {
+	capslock = "overload(control, esc)";
+      };
+    };
   };
 }
